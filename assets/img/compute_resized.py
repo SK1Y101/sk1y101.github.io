b0281c7f64e8d9e7289aa00dcf945fb64850df55
SK1Y101 <skiy@outlook.com>
2021-12-18 15:18:37 +0000

    image tomfoolery

--- assets/img/compute_resized.py
+++ assets/img/compute_resized.py
@@ -2,19 +2,77 @@
 from PIL import Image
 import os
 
+# search for any object within a string
+def contains(string, *args, search_and=False):
+    # search for the strings in the main string
+    search = [(arg in string) for arg in args]
+    # if we only want to return when all are present
+    if search_and:
+        # then return false is False is within the serahc
+        return not (False in search)
+    # otherwise, return if any is true
+    return True in search
+
+# search for a set of attributes in an xml file
+def search(xml, *args):
+    # split at the location of the argument, and then return whatever is within the quotations
+    return [xml.split(arg)[1].split('"')[1] for arg in args]
+
+# convert a nested list to a single list
+def nested_to_linear(lis):
+    out = []
+    for x in lis:
+        if isinstance(x, list):
+            for y in nested_to_linear(x):
+                out.append(y)
+            continue
+        out.append(x)
+    return out
+
+# convert a list of strings to only numeric
+def tonum(*args):
+    return [float("".join((x for x in string if (x.isdigit() or (x=="."))))) for string in nested_to_linear(args)]
+
 # compile a list of images in this folder
 def fetchNames():
     # fetch files
     files = [file for file in os.listdir() if os.path.isfile(file)]
     # Filter to only images
-    files = [file for file in files if ((".png" in file) or (".jpg" in file))]
+    files = [file for file in files if contains(file, ".png", ".jpg", ".svg")]
     # and return
     return files
 
-# fetch the size of each image
-def reSize(imgs):
+# rename svg's so they work
+def reName(imgs):
     # itterate over each image
     for img in imgs:
+        # Split the new name so it can be used later
+        name = img.split(".")
+        # fetch the file details
+        with open(img, "r") as file:
+            m = file.read()
+        # and read the dimensions
+        h, w = tonum(search(m, "height", "width"))
+        # list of sizes
+        size = [480, 800, 1400]
+        # itterate over the sizes
+        for _w in size:
+            # compute new height
+            _h = int(h * _w / w)
+            # Compute the new name
+            newname = name[0] + "-" + str(_w) + "x" + str(_h) + "." + name[1]
+            # and save as new
+            os.popen("cp "+img+" ../resized/"+newname)
+
+# fetch the size of each image and resize to our format
+def reSize(imgs):
+    svg = []
+    # itterate over each image
+    for img in imgs:
+        # don't work on svg's yet
+        if ".svg" in img:
+            svg.append(img)
+            continue
         # Split the new name so it can be used later
         name = img.split(".")
         # Fetch the image
@@ -33,6 +91,8 @@ def reSize(imgs):
             newname = name[0] + "-" + str(_w) + "x" + str(_h) + "." + name[1]
             # And save
             im_resize.save("../resized/"+newname)
+    #do the final work on the svg images
+    reName(svg)
 
 # Fetch images and resize them
 reSize(fetchNames())
