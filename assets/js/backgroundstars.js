1eb85d78f0cedc056812b5c8bee5d5acaa4abbbf
SK1Y101 <skiy@outlook.com>
2022-01-29 00:44:06 +0000

    f

---
+++ assets/js/backgroundstars.js
@@ -0,0 +1,16 @@
+// fetch the background canvas
+var background = document.getElementById("bgCanvas"),
+    bgCtx = background.getContext("2d"),
+    with = window.innerWidth,
+    height = document.body.offsetHeight;
+
+// ensure we have a minimum height
+(height < 400) ? height = 400 : height;
+
+// set the canvase size
+background.width = width;
+background.height = height;
+
+// draw the night sky
+bgCtx.fillStyle = "#005004C";
+bgCtx.fillRect(0, 0, width, height);
