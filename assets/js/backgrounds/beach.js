8a72678102e7b5751e1e439807a9288cd8ac1e98
Jack Lloyd-Walters <jack.lloyd-walters@canonical.com>
2025-04-09 10:35:29 +0100

    less opacity

--- assets/js/backgrounds/beach.js
+++ assets/js/backgrounds/beach.js
@@ -240,7 +240,7 @@ function smoothNoise(x, y, t) {
 function Wave(yBase) {
   this.yBase = yBase;
   this.amplitude = 10 + Math.random() * 15;
-  this.opacity = 0.1 + Math.random() * 0.1;
+  this.opacity = 0.05 + Math.random() * 0.5;
   this.colour = `rgba(255, 255, 255, ${this.opacity})`;
 }
 Wave.prototype.update = function (t) {
